65b41e2819440a52ca44c3ae09167c9c381e0ffd
Nikola Patafta <nikola.patafta@mev.hr>
2019-12-03 09:27:17 +0100

    Add project files.

---
+++ Kalendar-obaveza-studenti/Pages/Index.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Kalendar_obaveza_studenti.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
